b2e7f4c90bce71295851be06b75415f68b5565a9
Stephen Northrop <129760995+SteveN-ONS@users.noreply.github.com>
2024-10-18 14:43:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,52 +1,52 @@
-// create web server
-// http.createServer(function(req, res) {
-// get request url
-var url = req.url;
-// get request method
-var method = req.method;
-// url parse
-var urlParsed = url.parse(url, true);
-// get query string
-var query = urlParsed.query;
-// get pathname
-var pathname = urlParsed.pathname;
-// switch pathname
-switch(pathname) {
-    case '/':
-        // send response
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('Hello world');
-        break;
-    case '/comment':
-        // switch method
-        switch(method) {
-            case 'GET':
-                // send response
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(comments));
-                break;
-            case 'POST':
-                // get post data
-                var postData = '';
-                req.on('data', function(chunk) {
-                    postData += chunk;
-                });
-                req.on('end', function() {
-                    // parse post data
-                    var post = querystring.parse(postData);
-                    // add comment
-                    comments.push(post);
-                    // send response
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end(JSON.stringify(comments));
-                });
-                break;
-            default:
-                break;
-        }
-        break;
-    default:
-        break;
-}
-}).listen(3000, 'localhost');
-console.log('Server is running at http://localhost:3000');
+// // create web server
+// // http.createServer(function(req, res) {
+// // get request url
+// var url = req.url;
+// // get request method
+// var method = req.method;
+// // url parse
+// var urlParsed = url.parse(url, true);
+// // get query string
+// var query = urlParsed.query;
+// // get pathname
+// var pathname = urlParsed.pathname;
+// // switch pathname
+// switch(pathname) {
+//     case '/':
+//         // send response
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.end('Hello world');
+//         break;
+//     case '/comment':
+//         // switch method
+//         switch(method) {
+//             case 'GET':
+//                 // send response
+//                 res.writeHead(200, {'Content-Type': 'application/json'});
+//                 res.end(JSON.stringify(comments));
+//                 break;
+//             case 'POST':
+//                 // get post data
+//                 var postData = '';
+//                 req.on('data', function(chunk) {
+//                     postData += chunk;
+//                 });
+//                 req.on('end', function() {
+//                     // parse post data
+//                     var post = querystring.parse(postData);
+//                     // add comment
+//                     comments.push(post);
+//                     // send response
+//                     res.writeHead(200, {'Content-Type': 'application/json'});
+//                     res.end(JSON.stringify(comments));
+//                 });
+//                 break;
+//             default:
+//                 break;
+//         }
+//         break;
+//     default:
+//         break;
+// }
+// }).listen(3000, 'localhost');
+// console.log('Server is running at http://localhost:3000');
